0d6a545a1a0c1a71d8faf38b87082b2c7acd9b18
FilipMojto <fmojto@gmail.com>
2025-12-09 23:15:35 +0100

    perform feature engineering in 06_feature_engineering, define logging logic for all notebooks

--- notebooks/constants.py
+++ notebooks/constants.py
@@ -13,9 +13,24 @@ NUMERIC_FEATURES = [
     "complexity_delta",
     "max_func_change",
     "time_since_last_change",
-    "recent_churn"
+    "recent_churn",
 ]
-EMBEDDINGS = [
-    'code_embed',
-    "msg_embed"
+EMBEDDINGS = ["code_embed", "msg_embed"]
+
+ENGINEERED_FEATURES = [
+    "loc_churn_ratio",
+    "activity_per_exp",
+    "loc_added_bucket",
+    "extreme_churn_flag",
+    "line_token_total",
 ]
+
+ENGINEERED_FEATURES.extend([feature + "_ratio" for feature in LINE_TOKEN_FEATURES])
+
+INTERACTION_FEATURES = ["loc_added", "loc_deleted", "hunks_count"]
+
+for i in range(len(INTERACTION_FEATURES)):
+    for j in range(i+1, len(INTERACTION_FEATURES)):
+        f1 = INTERACTION_FEATURES[i]
+        f2 = INTERACTION_FEATURES[j]
+        ENGINEERED_FEATURES.append(f"{f1}_x_{f2}")
